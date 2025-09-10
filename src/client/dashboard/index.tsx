66dce246754222bee38671026d5b14fe614aab7a
Ryan Prendergast <rprendergast1121@gmail.com>
2025-09-10 11:45:41 -0400

    Cleanup

--- src/client/dashboard/index.tsx
+++ src/client/dashboard/index.tsx
@@ -1,6 +1,5 @@
 import { render } from "solid-js/web";
 import Dashboard from "./Dashboard";
-import "../styles/index.css";
 import "../styles/app.css";
 
 render(() => <Dashboard />, document.getElementById("root")!);
--- src/client/login/index.tsx
+++ src/client/login/index.tsx
@@ -1,6 +1,5 @@
 import { render } from "solid-js/web";
 import LoginPage from "./LoginPage";
-import "../styles/index.css";
 import "../styles/app.css";
 
 render(() => <LoginPage />, document.getElementById("root")!);
