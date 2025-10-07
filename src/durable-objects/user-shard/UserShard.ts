7a6254b2276838e21671501d818dc957dbe182f4
Ryan Prendergast <rprendergast1121@gmail.com>
2025-10-07 14:24:19 -0400

    User shard has access to d1 bindings

--- src/durable-objects/user-shard/UserShard.ts
+++ src/durable-objects/user-shard/UserShard.ts
@@ -1,9 +1,9 @@
 import { DurableObject } from "cloudflare:workers";
 import { drizzle, DrizzleSqliteDODatabase } from "drizzle-orm/durable-sqlite";
-import { eq, inArray } from "drizzle-orm";
 import { migrate } from "drizzle-orm/durable-sqlite/migrator";
 import migrations from "../../../user-shard-drizzle/migrations";
 import * as schema from "../../db/userShard.schema";
+import { drizzle as drizzleD1, DrizzleD1Database } from "drizzle-orm/d1";
 
 type UserShardEnv = {
   DB: D1Database;
@@ -13,13 +13,15 @@ type UserShardEnv = {
 };
 
 export class UserShard extends DurableObject<UserShardEnv> {
-  private db: DrizzleSqliteDODatabase<typeof schema>;
+  private shardDb: DrizzleSqliteDODatabase<typeof schema>;
+  private d1Db: DrizzleD1Database;
   private storage: DurableObjectStorage;
 
   constructor(ctx: DurableObjectState, env: UserShardEnv) {
     super(ctx, env);
     this.storage = ctx.storage;
-    this.db = drizzle(this.storage, { schema, logger: true });
+    this.shardDb = drizzle(this.storage, { schema, logger: true });
+    this.d1Db = drizzleD1(env.DB);
 
     // Run migrations before accepting any requests
     void ctx.blockConcurrencyWhile(async () => {
@@ -28,6 +30,6 @@ export class UserShard extends DurableObject<UserShardEnv> {
   }
 
   private async migrate() {
-    await migrate(this.db, migrations);
+    await migrate(this.shardDb, migrations);
   }
 }
