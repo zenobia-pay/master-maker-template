7ec7fc67f59d471cea74b8c9248f9f36c866092e
Ryan Prendergast <rprendergast1121@gmail.com>
2025-10-25 16:01:45 -0400

    Get rid of the user shard db

--- drizzle-do.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  out: "./user-shard-drizzle",
-  schema: "./src/db/userShard.schema.ts",
-  dialect: "sqlite",
-  driver: "durable-sqlite",
-});
--- src/durable-objects/user-shard/UserShard.ts
+++ src/durable-objects/user-shard/UserShard.ts
@@ -1,28 +1,11 @@
 import { DurableObject } from "cloudflare:workers";
-import { drizzle, DrizzleSqliteDODatabase } from "drizzle-orm/durable-sqlite";
-import { migrate } from "drizzle-orm/durable-sqlite/migrator";
-import migrations from "../../../user-shard-drizzle/migrations";
-import * as schema from "../../db/userShard.schema";
 import { drizzle as drizzleD1, DrizzleD1Database } from "drizzle-orm/d1";
 
 export class UserShard extends DurableObject<Env> {
-  private shardDb: DrizzleSqliteDODatabase<typeof schema>;
   private db: DrizzleD1Database;
-  private storage: DurableObjectStorage;
 
   constructor(ctx: DurableObjectState, env: Env) {
     super(ctx, env);
-    this.storage = ctx.storage;
-    this.shardDb = drizzle(this.storage, { schema, logger: true });
     this.db = drizzleD1(env.DB);
-
-    // Run migrations before accepting any requests
-    void ctx.blockConcurrencyWhile(async () => {
-      await this.migrate();
-    });
-  }
-
-  private async migrate() {
-    await migrate(this.shardDb, migrations);
   }
 }
