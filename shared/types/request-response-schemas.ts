ce7fad7c58d0e028517c6377e906cab2f690505b
Ryan Prendergast <rprendergast1121@gmail.com>
2025-11-05 17:04:25 -0500

    Error response

--- shared/types/request-response-schemas.ts
+++ shared/types/request-response-schemas.ts
@@ -4,6 +4,7 @@ export const ErrorResponseSchema = z.object({
   success: z.literal(false),
   message: z.string(),
   statusCode: z.number(),
+  error: z.string().optional(),
 });
 
 export type ErrorResponse = z.infer<typeof ErrorResponseSchema>;
