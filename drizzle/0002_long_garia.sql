068749076631408dd8f5af27fa6d544f1b1f378e
Theodore Li <teddy@zenobiapay.com>
2026-01-19 14:16:36 -0800

    Remove migration files

--- drizzle/0002_long_garia.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `user_properties` (
-	`user_id` text PRIMARY KEY NOT NULL,
-	`lorem` text,
-	`ipsum` text,
-	FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON UPDATE no action ON DELETE cascade
-);
